c5c0c8bd2bfe8355c39f1991d24c5ef35d32e83c
penyess <penyess@gmail.com>
2018-11-25 20:48:29 -0200

    Ajuste Scroll Menu Mobile

--- js/site.js
+++ js/site.js
@@ -3,13 +3,24 @@
 }
 
 	(function ($, window, document){
-		$(function () {
+		$(document).ready(function(){
 
 			$("#toggle_menu").click( function (){
 				$(".box-menu").toggleClass("ativo");
 			})
 
+			$(".box-menu p").click(function(evt){
+				console.log("1");
+				
 
+				evt.preventDefault();
+				$("html, body").stop().animate({
+					scrollTop: $( $(this).attr("data-section") ).offset().top - 63 + 'px'
+				}, 1000);
+
+				$(".box-menu").toggleClass("ativo");
+
+			});
 		})
 	})
 );
